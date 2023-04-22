847ce3003db2a4b82e3f7823cb3cc4231954ab3a
John Butler <jbutler@tripadvisor.com>
2023-04-22 12:24:57 -0400

    add farmer dialogue

--- src/game/scenes/TavernFarmer.tsx
+++ src/game/scenes/TavernFarmer.tsx
@@ -23,7 +23,25 @@ export const TavernFarmer = ({}: Props) => {
     content = (
       <>
         <p>
-          Help, monsters!
+          "I'm telling you, my cow's guts were completely ripped out! I was finding bits and pieces of them nearly all the way back to the old church.
+        </p>
+        <p>
+          Third time in the last month!
+        </p>
+        <p>
+          Whatever it was, it sure can eat quick. I came running the moment I heard the ruckus, but it had gotten what it came for by the time I reached the scene.
+        </p>
+        <p>
+          Seems the only way to stop this is to go find the dang thing where it sleeps.  Sure am glad you agreed to the job - you really are a swell kid!
+        </p>
+        <p>
+          Here's your advance on the job - go get yourself whatever you need to get started.
+        </p>
+        <p>
+          Come find me back here when you've cleaned out the old catacomb of whatever it is that's down there, and there's 20 more gold coins waiting for you.
+        </p>
+        <p>
+          Good luck!"
         </p>
         <p>
           <Link

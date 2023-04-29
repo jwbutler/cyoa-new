4a334ff67f88dfcd6dca477a1111e50eee91fa7d
John Butler <jbutler@tripadvisor.com>
2023-04-28 23:55:58 -0400

    fix

--- src/game/scenes/town/Tavern.tsx
+++ src/game/scenes/town/Tavern.tsx
@@ -2,26 +2,33 @@ import { Scene } from '../../../ui/components/Scene';
 import { Link } from '../../../ui/components/Link';
 import { Links } from '../../../ui/components/Links';
 import { SceneName } from '../../types';
+import { Column } from '../../../ui/components/Column';
+import { Columns } from '../../../ui/components/Columns';
 
 export const Tavern = () => {
   return (
     <Scene title="Tavern">
-      <p>
-        You're in a dimly lit tavern.  It's mostly empty,
-        but there are a few patrons in the back of the room.
-      </p>
+      <Columns>
+        <Column />
+        <Column>
+          <p>
+            You're in a dimly lit tavern. It's mostly empty,
+            but there are a few patrons in the back of the room.
+          </p>
 
-      <Links>
-        <Link to={SceneName.TAVERN_BARTENDER}>
-          Talk to the bartender
-        </Link>
-        <Link to={SceneName.TAVERN_FARMER}>
-          Talk to the farmer in the corner
-        </Link>
-        <Link to={SceneName.TOWN}>
-          Back to Town
-        </Link>
-      </Links>
+          <Links>
+            <Link to={SceneName.TAVERN_BARTENDER}>
+              Talk to the bartender
+            </Link>
+            <Link to={SceneName.TAVERN_FARMER}>
+              Talk to the farmer in the corner
+            </Link>
+            <Link to={SceneName.TOWN}>
+              Back to Town
+            </Link>
+          </Links>
+        </Column>
+      </Columns>
     </Scene>
   );
 };
--- src/game/scenes/town/TavernBartender.tsx
+++ src/game/scenes/town/TavernBartender.tsx
@@ -1,6 +1,7 @@
 import { Scene } from '../../../ui/components/Scene';
 import { Link } from '../../../ui/components/Link';
 import { Links } from '../../../ui/components/Links';
+import { SceneName } from '../../types';
 
 export const TavernBartender = () => (
   <Scene title="Tavern">
